75fca48baa91b98934bbf917bb008d670334642c
ayoub omari <omari.ayoub90@gmail.com>
2019-10-21 18:32:37 +0300

    adding timeline

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -1,32 +1,37 @@
 $(document).ready(function () {
 
+  // Set the date we're counting down to
+  timeStartfrom = $('#timeStartfrom').val();
+  var countDownDate = new Date(timeStartfrom).getTime();
+
+  // Update the count down every 1 second
+  var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+
+    $('.seconds').html(seconds);
+    $('.hours').html(hours);
+    $('.minutes').html(minutes);
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+
+
+
 })
-
-$('.expandMem').click(function () {
-  $(this).toggleClass('rotateLeft');
-  if ($(window).width() > 767) {
-    $(this).parents('.row').next('.members-row').slideToggle(200);
-  } else {
-    $(this).parents('.row').next('.members-row').fadeToggle(100);
-  }
-})
-
-
-$('.single-field').click(function () {
-  if ($('.single-field.selected').length < 4) {
-    $(this).toggleClass('selected');
-  } else {
-    $(this).removeClass('selected');
-  }
-
-});
-
-
-
-function toggleSearch(el) {
-  if ($(window).width() > 767) {
-    $(el).slideToggle(200);
-  } else {
-    $(el).fadeToggle(150);
-  }
-}
